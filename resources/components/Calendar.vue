47f24dad5337c8da01ec80e85d80a45ea95c0036
exaricode <82609830+exaricode@users.noreply.github.com>
2022-09-08 12:32:48 +0200

    Calendar component

--- resources/components/Calendar.vue
+++ resources/components/Calendar.vue
@@ -12,9 +12,26 @@
                 {{ day }}
             </div>
         </header>
-        <section>
-            <div>
-                {{ firstOfMonth }}
+        <section class="col-span-7">
+            <div class="w-full h-screen grid grid-cols-7 grid-rows-[repeat(7,_minmax(0,_1fr))]">
+                <day-container  v-for="i in firstOfMonth" 
+                        class="bg-slate-400"
+                        :date="new Date(currentYear, previousMonth, (numDaysPreviousMonth + i - firstOfMonth))">
+                    <template #day>
+                        {{ numDaysPreviousMonth + i - firstOfMonth }}
+                    </template>
+                </day-container>
+                <day-container v-for="j in numDaysOfMonth" class="bg-slate-50">
+                    <template #day>
+                        {{ j }}
+                    </template>
+                </day-container> 
+                <day-container v-for="k in (49 - numDaysOfMonth - firstOfMonth )" 
+                            class="bg-slate-400">
+                    <template #day>
+                        {{ k }}
+                    </template>
+                </day-container>
             </div>
         </section>
     </main>
@@ -23,28 +40,46 @@
 <script setup>
 import { ref, onBeforeMount } from 'vue';
 import NavBar from './navigation/NavBar.vue';
+import DayContainer from './calendar/DayContainer.vue';
+
+const numDaysOfMonth = ref();
+const numDaysPreviousMonth = ref();
 
 const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-const months = ['january', 'february', 'march', 'april', 'may', 'june', 'juli', 'august', 'september', 'oktober', 'november', 'december'];
+const months = [{name: 'january', days: 31},
+                {name: 'february', days: 29},
+                {name: 'march', days: 31},
+                {name: 'april', days: 30},
+                {name: 'may', days: 31},
+                {name: 'june', days: 30},
+                {name: 'juli', days: 31},
+                {name: 'august', days: 31}, 
+                {name: 'september', days: 30},
+                {name: 'oktober', days: 31 },
+                {name: 'november', days: 30 },
+                {name: 'december', days: 31 }];
 
 const currentYear = ref();
 const currentMonth = ref();
 const currentDate = ref();
 const currentDay = ref();
 const firstOfMonth = ref();
+const previousMonth = ref();
+const nextMonth = ref();
 
 onBeforeMount(() => {
     let date = new Date();
+
     currentYear.value = date.getFullYear();
-    currentMonth.value = months[date.getMonth()];
+    currentMonth.value = months[date.getMonth()].name;
     currentDate.value =  date.getDate();
     currentDay.value = date.getDay() === 0 ? days[days.length - 1] : days[date.getDay() - 1];
-    firstOfMonth.value = new Date(`1 ${currentMonth.value} ${currentYear.value}`);
-
-    // TODO: create function where to begin counting
+    firstOfMonth.value = new Date(`1 ${currentMonth.value} ${currentYear.value}`).getDay();
+    numDaysOfMonth.value = months[date.getMonth()].days;
+    previousMonth.value = months[date.getMonth() - 1].name;
+    numDaysPreviousMonth.value = months[date.getMonth() - 1].days;
+    nextMonth.value = months[date.getMonth() + 1].name;
 });
-
-
 </script>
 
 <style lang="scss" scoped>
---
+++ resources/components/calendar/DayContainer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="text-center border-2 border-black border-solid font-bold
+        hover:scale-105" :style="styleObject" data-day="">
+        <slot name="day"></slot>
+        {{props.date}}
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const styleObject = ref({
+    textShadow: '.5px .5px 2px #050505, -.5px -.5px 2px #050505',
+    fontSize: '1.5rem'
+});
+
+const props = defineProps({
+    date: String
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
