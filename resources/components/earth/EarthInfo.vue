15b84cb80d2bbdc8c52a13949d28d85c7b973b93
ari <82609830+exaricode@users.noreply.github.com>
2022-10-12 07:08:41 +0200

    Edit: move earthinfo to seperate file

--- resources/components/earth/EarthInfo.vue
+++ resources/components/earth/EarthInfo.vue
@@ -25,18 +25,15 @@
 </template>
 
 <script setup>
-import { ref, onMounted, h } from 'vue';
+import { ref, onMounted, onBeforeMount } from 'vue';
 import EarthInfoSection from './EarthInfoSection.vue';
+import earthInformation from '../../js/classes/earthInfo.js';
 
+const earthInfo = ref(earthInformation);
 const arrOne = ref([]);
 const arrTwo = ref([]);
 const arrThree = ref([]);
-onMounted(() => {
-    // get the 3 subGrid divs
-    const subGridOne = ref(document.getElementById('one'));
-    const subGridTwo = ref(document.getElementById('two'));
-    const subGridThree = ref(document.getElementById('three'));
-
+onBeforeMount(() => {
     const heightOne = ref(0);
     const heightTwo = ref(0);
     const heightThree = ref(0);
@@ -60,101 +57,6 @@ onMounted(() => {
             heightThree.value += earthInfo.value[e].length;
         }
     }
-})
-
-const earthInfo = ref({
-    measurements: [
-        {
-            name: 'Radius',
-            stat: '6.371km'
-        },
-        {
-            name: 'Surface',
-            stat: '510,000,000 km<sup>2</sup>'
-        },
-        {
-            name: 'Volume',
-            stat: '1,000,000,000,000 km<sup>3</sup>'
-        }
-    ],
-    rotation: [
-        {
-            name: 'Earth spin',
-            stat: '1600 km/h'
-        },
-        {
-            name: 'Length of day',
-            stat: '23h 56min 4sec'
-        }
-    ], 
-    age: [
-        {
-            name: 'Age',
-            stat: '&plusmn; 4.54 billion years old'
-        }
-    ],   
-    distance: [
-        {
-            name: 'To the moon',
-            stat: '384.400 km'
-        },
-        {
-            name: 'To the sun',
-            stat: '149,600,000 km'
-        }
-    ],
-    water: [
-        {
-            name: 'Total',
-            stat: '70% of the surface'
-        },
-        {
-            name: 'Salt water',
-            stat: '97%'
-        },
-        {
-            name: 'Fresh water',
-            stat: '3%'
-        }
-    ],
-    layers: [
-        {
-            name: 'Crust',
-            stat: '0 - 35 km'
-        },
-        {
-            name: 'Mantle',
-            stat: '35 - 2,890 km'
-        },
-        {
-            name: 'Core',
-            stat: '2,890 - 6,360 km'
-        }
-    ],
-    temperature: [
-        {
-            name: 'Core temperature',
-            stat: '5,500 <sup>o</sup>C'
-        },
-        {
-            name: 'Surface',
-            stat: '-88 to 58 <sup>o</sup>C'
-        }
-    ],
-    shape: [
-        {
-            name: 'Shape',
-            stat: 'Oblate Spheroid'
-        },
-        {
-            name: 'Semi-major axis',
-            stat: '6,378,137.0 m'
-        },
-        {
-            name: 'Semi-minor axis',
-            stat: '6,356,752.3 m'
-        }
-    ]
 })
 </script>
 
---
+++ resources/js/classes/earthInfo.js
@@ -0,0 +1,46 @@
+class EarthInfo {
+    constructor(name, stat) {
+        this.name = name;
+        this.stat = stat;
+    }
+}
+
+const earthInformation = {
+    measurements:[
+        new EarthInfo('Radius', '6.371 km'),
+        new EarthInfo('Surface', '510,000,000 km<sup>2</sup>'),
+        new EarthInfo('Volume', '1,000,000,000,000 km<sup>3</sup>')
+    ],
+    rotation: [
+        new EarthInfo('Earth spin', '1600 km/h'),
+        new EarthInfo('Length of day', '23h 56min 4sec')
+    ], 
+    age: [
+        new EarthInfo('Age', '&plusmn; 4.54 billion years old')
+    ],   
+    distance: [
+        new EarthInfo('To the moon', '384.400 km'),
+        new EarthInfo('To the sun', '149,600,000 km') 
+    ],
+    water: [
+        new EarthInfo('Total', '70% of the surface'),
+        new EarthInfo('Salt water', '97%'),
+        new EarthInfo('Fresh water', '3%') 
+    ],
+    layers: [
+        new EarthInfo('Crust', '0 - 35 km'),
+        new EarthInfo('Mantle', '35 - 2,890 km'),
+        new EarthInfo('Core', '2,890 - 6,360 km') 
+    ],
+    temperature: [
+        new EarthInfo('Core temperature', '5,500 <sup>o</sup>C'),
+        new EarthInfo('Surface', '-88 to 58 <sup>o</sup>C') 
+    ],
+    shape: [
+        new EarthInfo('Shape', 'Oblate Spheroid'),
+        new EarthInfo('Semi-major axis', '6,378,137.0 m'),
+        new EarthInfo('Semi-minor axis', '6,356,752.3 m')
+    ]
+}
+
+export default earthInformation;
