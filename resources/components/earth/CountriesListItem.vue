d8c2903b6d283409380b4d59a376ff006d53b1ac
ari <82609830+exaricode@users.noreply.github.com>
2022-11-28 08:06:30 +0100

    style change

--- resources/components/earth/CountriesListItem.vue
+++ resources/components/earth/CountriesListItem.vue
@@ -14,20 +14,24 @@ li {
     padding: .2rem;
 }
 
-li > span:first-of-type{
-    padding-inline-end: .2rem;
+li > span {
     display: block;
     text-align: center;
 }
 
 hr {
     height: .3rem;
-    background-image: linear-gradient(white, black, white);
+    background-image: linear-gradient(white, rgb(103, 232, 249), white);
     border-radius: 100% / 100%;
     border: none;
 }
+
+li > span:first-of-type {
+    padding-inline-end: .2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+}
 li > span:last-of-type {
     padding-inline-start: .2rem;
-
 }
 </style>
